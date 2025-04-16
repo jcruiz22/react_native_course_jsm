72f4c05bcc668712106d9817e2a3b5874c27df43
jcruiz22 <jcruizm22@gmail.com>
2025-04-16 23:24:24 +0200

    Refactor Search component to implement movie listing with FlatList and add SearchBar for improved user experience

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,13 +1,66 @@
+import MovieCard from '@/components/MovieCard';
+import { images } from '@/constants/images';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
+import { useRouter } from 'expo-router';
+import useFetch from '@/services/useFetch';
+import { fetchMovies } from '@/services/api';
+import { icons } from '@/constants/icons';
+import SearchBar from '@/components/SearchBar';
 
-const Search: React.FC = () => {
+const Search = () => {
+    const router = useRouter();
+
+    const {
+        data: movies,
+        loading: moviesLoading,
+        error: moviersError
+    } = useFetch(() => fetchMovies({
+        query: '',
+    }))
+
     return (
-        <View>
-            <Text>Search Screen</Text>
-        </View>
-    );
-};
+        <View className='flex-1 bg-primary'>
+            <Image
+                source={images.bg}
+                className='flex-1 absolute w-full z-0'
+                resizeMode='cover'
+            />
+            <FlatList
+                data={movies}
+                className="px-5"
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={2}
+                columnWrapperStyle={{
+                    justifyContent: 'center',
+                    marginBottom: 10,
+                    paddingRight: 5,
+                    gap: 20
+                }}
+                contentContainerStyle={{
+                    paddingBottom: 100,
+                }}
+                ListHeaderComponent={
+                    <>
+                    <View className='flex-row w-full justify-center items-center mt-20'>
+                        <Image
+                            source={icons.logo}
+                            className='w-12 h-10'
+                        />
+                    </View>
 
-const styles = StyleSheet.create({});
+                    <View className='my-5'>
+                        <SearchBar placeholder="Search for a movies..."/>
+                    </View>
+                    </>
+                }
+                
+            />
+
+
+        </View>
+    )
+}
+
 export default Search;
